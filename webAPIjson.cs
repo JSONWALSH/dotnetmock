fc5e05d1622ca82fcdb755e7696f6a3633cab980
JSONWALSH <jwalsh@live.ie>
2017-08-10 13:42:03 +0200

    Create webAPIjson.cs

---
+++ webAPIjson.cs
@@ -0,0 +1,12 @@
+        //GET json /api/values/getAirports?  all optional
+        [AllowAnonymous, ActionName("getAirports")]
+        public IEnumerable<string> getAirports([FromUri] int? pid = 0, [FromUri] string iata = null, [FromUri] string name = null, StringComparison? ignoreCase = null)
+        {
+            int pageid = pid ?? 0;
+            var caseType = (ignoreCase.HasValue && (int)ignoreCase < 6) ? ignoreCase : StringComparison.CurrentCultureIgnoreCase;
+
+            IList<Airport> airports = _repository.Get().DefaultIfEmpty().Where(m => m.Code.StartsWith(iata ?? m.Code, (StringComparison)caseType) 
+                                                                                 && m.Name.StartsWith(name ?? m.Name, (StringComparison)caseType)).ToList();
+
+            while (pageid < airports.Count) yield return JsonConvert.SerializeObject(new { Airport = airports[pageid++] });
+        }
